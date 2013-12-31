814c1aca10d82a7b24f25b7da2e3e4865df360e4
Andreas Öman <andreas@lonelycoder.com>
2013-12-31 10:16:26 +0100

    Don't crash if 'stable' track does not exist

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -7,13 +7,12 @@ class DownloadsController < ApplicationController
     @project = Project.find(params[:project_id])
   end
 
+
+
   def track
     @releases = Release.find(:all, :params => { :project => @project.identifier })
 
     @tracks = @releases.map {|t| { :title => t.title, :id => t.id, :active => t.id == params[:track] }}.compact
-    p @tracks
-
-
     tracklist = @releases.select { |t| t.id == params[:track] }
 
     if tracklist.length == 0
@@ -24,8 +23,15 @@ class DownloadsController < ApplicationController
   end
 
   def index
-    params[:track] = 'stable'
-    track
+    @releases = Release.find(:all, :params => { :project => @project.identifier })
+    if @releases.length == 0
+      render_404
+      return
+    end
+
+    @track = @releases[0]
+
+    @tracks = @releases.map {|t| { :title => t.title, :id => t.id, :active => t.id == @track.id }}.compact
     render :action => 'track'
   end
 
